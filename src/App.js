145f8b5fbb0a321ae9de7444ee7628a7ae3553cb
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2022-02-17 09:18:02 +0300

    some

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello World</h1>
+       <h1>Hello World </h1>
       </header>
     </div>
   );
